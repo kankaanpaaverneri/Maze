a9d9ec008d88ce9ef9590765ef88a146141c6254
Verneri Kankaanpää <verneri.kankaanpaa@gmail.com>
2025-01-16 00:45:07 +0200

    Add handle_game_end function for better user experience.

--- src/read_input.rs
+++ src/read_input.rs
@@ -10,9 +10,7 @@ pub mod user_input {
     use crossterm::{
         event::{read, Event, KeyCode},
         execute,
-        terminal::{
-            disable_raw_mode, enable_raw_mode, size, Clear, ClearType, ScrollDown, SetSize,
-        },
+        terminal::{disable_raw_mode, enable_raw_mode, size, Clear, ClearType, SetSize},
     };
 
     pub fn init_raw_terminal(
@@ -22,7 +20,8 @@ pub mod user_input {
     ) -> std::io::Result<()> {
         enable_raw_mode()?;
         let (columns, rows) = size()?;
-        execute!(std::io::stdout(), SetSize(200, 200), Clear(ClearType::All))?;
+        execute!(std::io::stdout(), SetSize(50, 50), Clear(ClearType::All))?;
+        print!("Press arrow keys to start moving around\r\n");
         start_event_loop(grid, list_of_moves, &player_original_position)?;
 
         execute!(std::io::stdout(), SetSize(columns, rows))?;
@@ -39,51 +38,34 @@ pub mod user_input {
                 Event::Key(key) => match key.code {
                     KeyCode::Up => {
                         let is_game_completed = move_player(grid, Input::Up);
-                        if !handle_event(
-                            grid,
-                            list_of_moves,
-                            player_original_position,
-                            is_game_completed,
-                        ) {
+                        if !handle_event(grid, list_of_moves, is_game_completed) {
+                            handle_game_end(grid, list_of_moves, player_original_position)?;
                             break;
                         }
                     }
                     KeyCode::Down => {
                         let is_game_completed = move_player(grid, Input::Down);
-                        if !handle_event(
-                            grid,
-                            list_of_moves,
-                            player_original_position,
-                            is_game_completed,
-                        ) {
+                        if !handle_event(grid, list_of_moves, is_game_completed) {
+                            handle_game_end(grid, list_of_moves, player_original_position)?;
                             break;
                         }
                     }
                     KeyCode::Left => {
                         let is_game_completed = move_player(grid, Input::Left);
-                        if !handle_event(
-                            grid,
-                            list_of_moves,
-                            player_original_position,
-                            is_game_completed,
-                        ) {
+                        if !handle_event(grid, list_of_moves, is_game_completed) {
+                            handle_game_end(grid, list_of_moves, player_original_position)?;
                             break;
                         }
                     }
                     KeyCode::Right => {
                         let is_game_completed = move_player(grid, Input::Right);
-                        if !handle_event(
-                            grid,
-                            list_of_moves,
-                            player_original_position,
-                            is_game_completed,
-                        ) {
+                        if !handle_event(grid, list_of_moves, is_game_completed) {
+                            handle_game_end(grid, list_of_moves, player_original_position)?;
                             break;
                         }
                     }
                     KeyCode::Esc => {
-                        add_character_to_grid(grid, START, player_original_position);
-                        print_full_grid_with_move_history(grid, list_of_moves);
+                        handle_game_end(grid, list_of_moves, player_original_position)?;
                         break;
                     }
                     _ => {}
@@ -91,24 +73,18 @@ pub mod user_input {
                 _ => {}
             }
         }
+        execute!(std::io::stdout(), Clear(ClearType::All))?;
         disable_raw_mode()?;
         Ok(())
     }
+
     fn handle_event(
         grid: &mut [[char; GRID_HEIGHT]; GRID_WIDTH],
         list_of_moves: &mut Vec<Coordinates>,
-        player_original_position: &Coordinates,
         is_game_complete: bool,
     ) -> bool {
-        if let Err(e) = execute!(std::io::stdout(), ScrollDown(5)) {
-            print!("{e}\r\n");
-            return false;
-        }
         print_limited_view(grid, 2);
         if is_game_complete {
-            add_character_to_grid(grid, START, player_original_position);
-            print_full_grid_with_move_history(grid, list_of_moves);
-            print!("Game complete\r\n");
             return false;
         }
 
@@ -116,5 +92,30 @@ pub mod user_input {
             list_of_moves.push(coordinates);
         }
         return true;
+    }
+
+    fn handle_game_end(
+        grid: &mut [[char; GRID_HEIGHT]; GRID_WIDTH],
+        list_of_moves: &mut Vec<Coordinates>,
+        player_original_position: &Coordinates,
+    ) -> std::io::Result<()> {
+        execute!(std::io::stdout(), SetSize(200, 200))?;
+        add_character_to_grid(grid, START, player_original_position);
+        print_full_grid_with_move_history(grid, list_of_moves);
+        print!("Press ESC key to quit\r\n");
+        loop {
+            let end_event = read()?;
+
+            match end_event {
+                Event::Key(key) => match key.code {
+                    KeyCode::Esc => {
+                        break;
+                    }
+                    _ => {}
+                },
+                _ => {}
+            }
+        }
+        Ok(())
     }
 }
